4f21bfe18a6cf0ce715cf14abcd86aaf137fa5b9
Allan <allanfelipedk@gmail.com>
2024-01-24 21:37:02 -0300

    creating an exercise repository in python

---
+++ ex004.py
@@ -0,0 +1,9 @@
+algo = input('Digite algo: ')
+print('------------')
+print(f'Tipo primitivo: {type(algo)}')
+print(f'É numero: {algo.isalnum()}')
+print(f'Esta em maiusculas: {algo.isupper()}')
+print(f'Esta em minusculas: {algo.islower()}')
+print(f'Esta capitalizada: {algo.istitle()}')
+
+
---
+++ ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+numero = int(input("Digite um numero: "))
+# sucessor = numero + 1
+# antecessor = numero - 1
+print(f"Analisando o valor {numero}, o seu antecessor é {numero -1} e o seu sucessor é {numero + 1}")
+
