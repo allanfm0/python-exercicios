1c4941611947ed6588f907efe6ecfbf00c0f7e15
Allan <allanfelipedk@gmail.com>
2024-01-30 20:31:11 -0300

    ex014

---
+++ ex014.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+c = float(input("Digite a temperatura em °C: "))
+f = ((9 * c) / 5) + 32
+print(f"A temperatura em {c}°C corresponde a {f}°F.")
